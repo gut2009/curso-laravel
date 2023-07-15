9571141ec1fa2b7edd1347da17f433b915360c52
Gut2009 <luiz.gutierrez2007@hotmail.com>
2023-07-15 12:55:47 -0300

    fornt-ends

---
+++ resources/views/components/navegacao.blade.php
@@ -0,0 +1,32 @@
+<nav id="sidebar-menu" class="col-md-3 col-lg-2 d-md-block bg-body-tertiary sidebar collapse">
+  <div class="position-sticky pt-3 sidebar-sticky">
+    <ul class="nav flex-column">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">
+          <span data-feather="home" class="align-text-bottom"></span>
+          Dashboard
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <span data-feather="file" class="align-text-bottom"></span>
+          Vendas
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <span data-feather="file" class="align-text-bottom"></span>
+          Produtos
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <span data-feather="file" class="align-text-bottom"></span>
+          Clientes
+        </a>
+      </li>
+    </ul>
+
+    <hr class="my-3">
+  </div>
+</nav>
